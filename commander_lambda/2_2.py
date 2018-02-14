3f7553f99efef1278eaa5760b1a0eece2d11d194
Myroslav Rozum <myroslav.rozum@nbcuni.com>
2018-02-14 12:04:23 -0500

    2

---
+++ commander_lambda/2_2.py
@@ -0,0 +1,8 @@
+def form_tuple(x):
+        return tuple([ int(i) for i in x.split('.') ])
+
+#versions = ["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"]
+versions = ["1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"]
+
+#print sorted(map(form_tuple, versions))
+print sorted(versions, key=form_tuple)
