094e7a5eb8b9c762ea14150288c6caa008601915
Myroslav Rozum <myroslavrozum@Myroslavs-Air.fios-router.home>
2018-09-19 22:19:46 -0400

    scripting manual steps

--- gke_workout/script.sh
+++ gke_workout/script.sh
@@ -1,11 +1,15 @@
-#src/gke-workout/depmanager/gke/
+#cd src/gke-workout/depmanager/gke/
 gcloud deployment-manager deployments create gke-workout --config=./gke_workout.yaml
 
 kubectl create namespace monitoring
 kubectl create -f config-map.yaml -n monitoring
-kubectl apply -f ./rbak-setup.yaml
+
+ACCOUNT='Myroslav.Rozum@gmail.com'
+kubectl create clusterrolebinding owner-cluster-admin-binding     --clusterrole cluster-admin     --user $ACCOUNT
+kubectl apply -f ./rbac-setup.yaml
+
 kubectl apply -f ./deployment.yaml
-kubectl expose deployment prometheus  --type=LoadBalancer --name=prometheus-service
+kubectl expose deployment prometheus-deployment --type=LoadBalancer --port=80 --target-port=9090 --name=prometheus-service --namespace=monitoring
 
 cd ../../build/
 gcloud builds submit --config=./cloudbuild_Dockerfile.yaml
