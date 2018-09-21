e409971be9c293d430e1e5f3c8d7902ceba52f42
Myroslav Rozum <myroslavrozum@Myroslavs-Air.fios-router.home>
2018-09-21 13:50:03 -0400

    scripting manual steps

--- gke_workout/script.sh
+++ gke_workout/script.sh
@@ -1,18 +1,17 @@
-cd src/gke-workout/depmanager/gke/
+cd ~/src/gke-workout/depmanager/gke/
 gcloud deployment-manager deployments create gke-workout --config=./gke_workout.yaml
 
-gcloud container clusters get-credentials gke-workout-cluster --zone us-east4-c --project gke-workout
+gcloud container clusters get-credentials gke-workout-cluster --zone us-east4-c --project sandbox-217104
 kubectl create namespace monitoring
-kubectl create -f config-map.yaml -n monitoring
+kubectl create -f kubeconfigs/config-map.yaml -n monitoring
 
 ACCOUNT='Myroslav.Rozum@gmail.com'
 kubectl create clusterrolebinding owner-cluster-admin-binding     --clusterrole cluster-admin     --user $ACCOUNT
-kubectl apply -f ./rbac-setup.yaml
+kubectl apply -f kubeconfigs/rbac-setup.yaml
 
-kubectl apply -f ./deployment.yaml
+kubectl apply -f kubeconfigs//deployment.yaml
 
-cd ../../../build/
-gsutil cp gs://gke-workout-scripts/prometheus.yml .
+cd ../../build/
 gcloud builds submit --config=./cloudbuild_Dockerfile.yaml
 
 kubectl expose deployment prometheus-deployment --type=LoadBalancer --port=80 --target-port=9090 --name=prometheus-service --namespace=monitoring
