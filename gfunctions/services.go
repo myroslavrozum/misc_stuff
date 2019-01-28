54a105334e5a12359bcdda76db432b7903255911
Myroslav Rozum <myroslavrozum@Myroslavs-Air.princeton.epam.com>
2019-01-28 14:15:21 -0500

    rm cors config

--- gfunctions/services.go
+++ gfunctions/services.go
@@ -17,7 +17,21 @@ import (
 type Service map[string]interface{}
 
 // HelloGet is an HTTP Cloud Function.
+// CORSEnabledFunction is an example of setting CORS headers.
+// For more information about CORS and CORS preflight requests, see
+// https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request.
+// https://cloud.google.com/functions/docs/writing/http#writing_http_helloworld-go
 func ServicesGet(w http.ResponseWriter, r *http.Request) {
+  // Set CORS headers for the preflight request
+  if r.Method == http.MethodOptions {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+    w.Header().Set("Access-Control-Max-Age", "3600")
+    w.WriteHeader(http.StatusNoContent)
+      return
+  }
+
   var myMapSlice []Service
   //https://golang.org/pkg/encoding/json/
   myMapSlice = append(myMapSlice,
