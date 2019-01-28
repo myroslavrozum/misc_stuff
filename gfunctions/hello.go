c2918d3d1c6c27b860ef0a927c13a005944fbdd4
Myroslav Rozum <myroslavrozum@Myroslavs-Air.princeton.epam.com>
2019-01-28 11:53:51 -0500

    json add

---
+++ gfunctions/hello.go
@@ -0,0 +1,40 @@
+// Copyright 2018 Google LLC. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// [START functions_helloworld_get]
+
+// Package helloworld provides a set of Cloud Function samples.
+package helloworld
+
+import (
+        "fmt"
+        "net/http"
+        "encoding/json"
+)
+
+// HelloGet is an HTTP Cloud Function.
+func HelloGet(w http.ResponseWriter, r *http.Request) {
+  	const jsonStream = `
+	{"Name": "Ed", "Text": "Knock knock."}
+	{"Name": "Sam", "Text": "Who's there?"}
+	{"Name": "Ed", "Text": "Go fmt."}
+	{"Name": "Sam", "Text": "Go fmt who?"}
+	{"Name": "Ed", "Text": "Go fmt yourself!"}
+`
+	type Message struct {
+		Name, Text string
+	}
+	dec := json.NewDecoder(strings.NewReader(jsonStream))
+	for {
+		var m Message
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+    fmt.Fprint(w,"%s: %s\n", m.Name, m.Text)
+	}
+}
+
+// [END functions_helloworld_get]
